1c2d6481b7aa8e872dca3893ff84ce6e669f4f7b
Jeffrey <jeffreylim@outlook.com>
2018-10-01 14:09:14 -0400

    Added HandTest

---
+++ src/test/java/HandTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase {
+	
+	public void testSampleHand() {
+		Card card1 = new Card("SQ");
+		Card card2 = new Card("CJ");
+		Card card3 = new Card("D10");
+		Card card4 = new Card("C2");
+		Card card5 = new Card("CA");
+		Hand h = new Hand(card1, card2, card3, card4, card5);
+		
+		assertNotNull(h);
+	}
+	
+	public void testGetCardAtIndex() {
+		Card card1 = new Card("DK");
+		Card card2 = new Card("CJ");
+		Card card3 = new Card("SA");
+		Card card4 = new Card("SQ");
+		Card card5 = new Card("H9");
+		Hand h = new Hand(card1, card2, card3, card4, card5);
+		
+		assertEquals("Jack of Clubs", h.getCardAtIndex(1).toString());
+	}
+}
