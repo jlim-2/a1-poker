99567c3d276d4efa3b9310e9ed86aef4086f05ff
Jeffrey <jeffreylim@outlook.com>
2018-10-01 13:39:04 -0400

    Refactoring

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -11,13 +11,91 @@ public class Card {
 	}
 	
 	public Boolean compareRank(Card card) {
-		
-		return false;
+		if (card.getRankInt() > rank) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public Boolean compareSuit(Card card) {
+		if (card.getSuitInt() > suit) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public int convertSuitStringToInt(Card card) {
+		String tempSuit = card.getSuitString();
+		System.out.println(tempSuit);
 		
-		return false;
+		switch (tempSuit) {
+			case "S":	suit = 4;
+						break;
+						
+			case "H":	suit = 3;
+						break;
+						
+			case "C":	suit = 2;
+						break;
+						
+			case "D":	suit = 1;
+						break;
+						
+			default:	suit = 0;
+		}
+		
+		return suit;
+	}
+	
+	public int convertRankStringToInt(Card card) {
+		String tempRank = card.getRankString();
+		
+		switch(tempRank) {
+		case "A":	rank = 14;
+					break;
+					
+		case "K":	rank = 13;
+					break;
+					
+		case "Q":	rank = 12;
+					break;
+					
+		case "J":	rank = 11;
+					break;
+					
+		case "10":	rank = 10;
+					break;
+					
+		case "9":	rank = 9;
+					break;
+					
+		case "8":	rank = 8;
+					break;
+					
+		case "7":	rank = 7;
+					break;
+		
+		case "6":	rank = 6;
+					break;
+					
+		case "5":	rank = 5;
+					break;
+					
+		case "4":	rank = 4;
+					break;
+					
+		case "3":	rank = 3;
+					break;
+					
+		case "2":	rank = 3;
+					break;
+					
+		default:	rank = 0;
+		}
+		
+		return rank;
 	}
 	
 	public String getSuitString() {
@@ -33,5 +111,13 @@ public class Card {
 		rankString = String.valueOf(rankChar);
 		
 		return rankString;
+	}
+	
+	public int getSuitInt() {
+		return suit;
+	}
+	
+	public int getRankInt() {
+		return rank;
 	}
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -5,21 +5,26 @@ public class CardTest extends TestCase{
 	public void testSampleCard() {
 		Card card = new Card("SK");
 		
+		card.convertSuitStringToInt(card);
+		card.convertRankStringToInt(card);
+		
 		assertEquals("K", card.getRankString());
 		assertEquals("S",  card.getSuitString());
+		assertEquals(4, card.getSuitInt());
+		assertEquals(13, card.getRankInt());
 	}
 	
 	public void testCompareRank() {
 		Card card1 = new Card("DK");
 		Card card2 = new Card("H10");
 		
-		assertTrue(card1.compareRank(card2));
+		assertFalse(card1.compareRank(card2));
 	}
 	
 	public void testCompareSuit() {
 		Card card1 = new Card("SA");
 		Card card2 = new Card("DA");
 		
-		assertTrue(card1.compareSuit(card2));
+		assertFalse(card1.compareSuit(card2));
 	}
 }
