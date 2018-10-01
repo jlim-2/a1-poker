e317c3a5c633263827c4af330ca2bf2c794974f8
Jeffrey <jeffreylim@outlook.com>
2018-10-01 13:46:03 -0400

    Refactoring Card.java

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -53,46 +53,46 @@ public class Card {
 		String tempRank = card.getRankString();
 		
 		switch(tempRank) {
-		case "A":	rank = 14;
-					break;
-					
-		case "K":	rank = 13;
-					break;
-					
-		case "Q":	rank = 12;
-					break;
-					
-		case "J":	rank = 11;
-					break;
-					
-		case "10":	rank = 10;
-					break;
-					
-		case "9":	rank = 9;
-					break;
-					
-		case "8":	rank = 8;
-					break;
-					
-		case "7":	rank = 7;
-					break;
-		
-		case "6":	rank = 6;
-					break;
-					
-		case "5":	rank = 5;
-					break;
-					
-		case "4":	rank = 4;
-					break;
-					
-		case "3":	rank = 3;
-					break;
-					
-		case "2":	rank = 3;
-					break;
-					
-		default:	rank = 0;
+			case "A":	rank = 14;
+						break;
+						
+			case "K":	rank = 13;
+						break;
+						
+			case "Q":	rank = 12;
+						break;
+						
+			case "J":	rank = 11;
+						break;
+						
+			case "10":	rank = 10;
+						break;
+						
+			case "9":	rank = 9;
+						break;
+						
+			case "8":	rank = 8;
+						break;
+						
+			case "7":	rank = 7;
+						break;
+			
+			case "6":	rank = 6;
+						break;
+						
+			case "5":	rank = 5;
+						break;
+						
+			case "4":	rank = 4;
+						break;
+						
+			case "3":	rank = 3;
+						break;
+						
+			case "2":	rank = 3;
+						break;
+						
+			default:	rank = 0;
 		}
 		
 		return rank;
@@ -119,4 +119,6 @@ public class Card {
 	public int getRankInt() {
 		return rank;
 	}
+	
+	
 }
