6bb0cf1610901567c5bac04bd958ec9ae1788198
Jeffrey <jeffreylim@outlook.com>
2018-10-04 00:20:26 -0400

    Refactoring

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,17 +1,29 @@
 
 public class Game {
 	private String welcomeMessage = "";
+	Hand compHand;
+	Hand oppHand;
 	
 	public Game() {
 		welcomeMessage = "Welcome to Poker!";
+		//compHand = new Hand(file);
+		//oppHand = new Hand(file);
 		run();
 	}
 	
-	public void getWelcomeMessage() {
+	public String getWelcomeMessage() {
 		return welcomeMessage;
 	}
 	
 	public void run() {
-		
+		System.out.println(welcomeMessage);
+	}
+	
+	public Hand getAIHand() {
+		return compHand;
+	}
+	
+	public Hand getOppHand() {		
+		return oppHand;
 	}
 }
