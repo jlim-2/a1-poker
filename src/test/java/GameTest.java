12ab9755ea8cbd2caa453d59dcb2d1adf063629b
Jeffrey <jeffreylim@outlook.com>
2018-10-06 09:27:11 -0400

    Added test to test if newGame method works

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertNotEquals;
+
 import junit.framework.TestCase;
 
 public class GameTest extends TestCase{
@@ -13,5 +15,14 @@ public class GameTest extends TestCase{
 		Game g = new Game();
 		
 		assertNotNull(g.getWelcomeMessage());
+	}
+	
+	public void testNewGame() {
+		Game g = new Game();
+		
+		Hand prevAIHand = g.getAIHand();
+		g.newGame();
+		
+		assertNotEquals(prevAIHand, g.getAIHand());		
 	}
 }
